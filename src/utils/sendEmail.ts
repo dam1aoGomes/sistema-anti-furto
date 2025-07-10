58d60ed06992c633cd1e0762289140cd6c02f323
Damiao Gomes <damiao28.contato@gmail.com>
2025-07-10 02:33:37 -0300

    Update sender email to use environment variable for SendGrid configuration

--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -8,7 +8,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
 export const sendAlertEmail = async (to: string, equipmentName: string) => {
   const msg = {
     to,
-    from: 'damiaoneto055@gmail.com', // precisa ser verificado no SendGrid
+    from: process.env.SENDGRID_FROM as string, // precisa ser verificado no SendGrid
     subject: `Alerta: Equipamento "${equipmentName}" foi removido do alcance`,
     text: `O equipamento "${equipmentName}" saiu da sala.`,
     html: `<strong>O equipamento "${equipmentName}" foi retirado da sala sem autorização ou supervisão.</strong>`,
